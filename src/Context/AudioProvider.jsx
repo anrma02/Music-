b9ba680a91dc6cf113b46c70ac395346d1f3b64a
anrma02 <antramcam2002@gmail.com>
2023-12-12 17:35:28 +0700

    Refactor AudioProvider component to handle audio duration, handle Position

--- src/Context/AudioProvider.jsx
+++ src/Context/AudioProvider.jsx
@@ -1,9 +1,7 @@
-
 import { createContext, useContext, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const AudioContext = createContext();
-
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const useAudio = () => {
@@ -15,6 +13,7 @@ export const AudioProvider = ({ children }) => {
      const audioRef = useRef(new Audio());
      const [audioUrl, setAudioUrl] = useState('');
      const [currentTime, setCurrentTime] = useState(0);
+     const [duration, setDuration] = useState(0);
 
      const playPauseToggle = (url) => {
           if (audioUrl === url) {
@@ -25,8 +24,9 @@ export const AudioProvider = ({ children }) => {
                }
           } else {
                setAudioUrl(url);
+               setCurrentTime(0);
                audioRef.current.src = url;
-               audioRef.current.currentTime = currentTime;
+               audioRef.current.currentTime = 0;
                audioRef.current.play();
           }
           setIsPlaying(!isPlaying);
@@ -36,25 +36,31 @@ export const AudioProvider = ({ children }) => {
           setCurrentTime(audioRef.current.currentTime);
      };
 
+     const handleLoadedData = () => {
+          setDuration(audioRef.current.duration);
+     };
 
      useEffect(() => {
           audioRef.current.addEventListener('timeupdate', handleTimeUpdate);
+          audioRef.current.addEventListener('loadeddata', handleLoadedData);
+
           return () => {
-               // eslint-disable-next-line react-hooks/exhaustive-deps
                audioRef.current.removeEventListener('timeupdate', handleTimeUpdate);
+               // eslint-disable-next-line react-hooks/exhaustive-deps
+               audioRef.current.removeEventListener('loadeddata', handleLoadedData);
           };
-     }, []);
+     }, [audioUrl]);
+
      const value = {
           isPlaying,
           audioUrl,
           playPauseToggle,
+          currentTime,
+          duration,
+          audioRef,
      };
 
-     return (
-          <AudioContext.Provider value={value}>
-               {children}
-          </AudioContext.Provider>
-     );
+     return <AudioContext.Provider value={value}>{children}</AudioContext.Provider>;
 };
 
 AudioProvider.propTypes = {
--- src/layout/Components/Footer/HandleButton.jsx
+++ src/layout/Components/Footer/HandleButton.jsx
@@ -1,80 +1,89 @@
+function formatDuration(value) {
+     const minute = Math.floor(value / 60);
+     const secondLeft = value - minute * 60;
+     return `${minute}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
+}
 import classNames from 'classnames/bind';
 import { AiOutlinePauseCircle, AiOutlinePlayCircle } from 'react-icons/ai';
-import { BiShuffle, } from 'react-icons/bi';
+import { BiShuffle } from 'react-icons/bi';
 import { TbPlayerTrackNext, TbPlayerTrackPrev } from 'react-icons/tb';
-import { IoRepeatSharp } from 'react-icons/io5'
+import { IoRepeatSharp } from 'react-icons/io5';
 import Slider from '@mui/material/Slider';
 import { useTheme } from '@mui/material/styles';
 
 
 import style from './Footer.module.scss';
-import React from 'react';
-import { useAudio } from '~/Context/AudioProvider';
 
+import { useAudio } from '~/Context/AudioProvider';
+import { useState } from 'react';
 
 const cx = classNames.bind(style);
 
+
+
+
 function HandleButton() {
-
-     const { isPlaying, playPauseToggle, audioUrl } = useAudio();
-     const position = 0;
-     const duration = 0;
-
+     const { isPlaying, playPauseToggle, audioUrl, currentTime, duration, audioRef } = useAudio();
+     const [sliderValue, setSliderValue] = useState(currentTime)
      const theme = useTheme();
-     function formatDuration(value) {
-          const minute = Math.floor(value / 60);
-          const secondLeft = value - minute * 60;
-          return `${minute}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
-     }
 
-
-
-     const handlePlay = () => {
-
+     const handPlay = () => {
+          playPauseToggle(audioUrl);
      };
 
      const handleNextSong = () => {
-
+          // Implement logic for next song
      };
 
      const handlePreviousSong = () => {
-
+          // Implement logic for previous song
      };
 
      const handleRepeatClick = () => {
-
+          // Implement logic for repeat click
      };
 
      const handleShuffleClick = () => {
-
+          // Implement logic for shuffle click
      };
 
-     const handleSeek = () => {
-
+     const handleSeek = (event, newValue) => {
+          // Pause the audio while seeking
+          audioRef.current.pause();
+          // Update the slider value
+          setSliderValue(newValue);
      };
+
+     const handleSeekEnd = () => {
+          // Set the audio position to the selected value
+          audioRef.current.currentTime = sliderValue;
+          // If the audio was playing, resume playback
+          if (isPlaying) {
+               audioRef.current.play();
+          }
+     };
+
 
      return (
           <>
-               <div className={cx("icon-play")}>
+               <div className={cx('icon-play')}>
                     <button className={cx('button')} onClick={handleRepeatClick}>
                          <BiShuffle className={cx('icon')} />
                     </button>
 
-                    {/* audio */}
-
-                    {/*  */}
                     <button onClick={handlePreviousSong}>
                          <TbPlayerTrackPrev className={cx('icon')} />
                     </button>
 
-                    <button onClick={playPauseToggle}>
-                         {isPlaying ?
+                    <button onClick={handPlay}>
+                         {isPlaying ? (
                               <AiOutlinePauseCircle className={cx('icon')} />
-                              :
-                              <AiOutlinePlayCircle className={cx('icon')} />}
+                         ) : (
+                              <AiOutlinePlayCircle className={cx('icon')} />
+                         )}
                     </button>
 
-                    <button onClick={handleNextSong} >
+                    <button onClick={handleNextSong}>
                          <TbPlayerTrackNext className={cx('icon')} />
                     </button>
 
@@ -82,17 +91,20 @@ function HandleButton() {
                          <IoRepeatSharp className={cx('icon')} />
                     </button>
                </div>
-               <div className={cx("flex justify-center px-2")}>
-                    <div className={cx("pr-4 text-[12px] flex items-center  text-small text-[#ebeaeac3]")}>{formatDuration(position)}</div>
+               <div className={cx('flex justify-center px-2')}>
+                    <div className={cx('pr-4 text-[12px] flex items-center text-small text-[#ebeaeac3]')}>
+                         {formatDuration(Math.floor(currentTime))}
+                    </div>
                     <Slider
                          aria-label="time-indicator"
                          size="small"
-                         value={position || 0}
+                         value={sliderValue}
                          min={0}
-
                          step={1}
                          max={duration}
                          onChange={handleSeek}
+                         onChangeCommitted={handleSeekEnd}
+                         onMouseDown={() => audioRef.current.pause()}
                          sx={{
                               color: theme.palette.mode === "#FFFBF5" ? "#7743DB" : "#FFFBF5",
                               height: 2,
@@ -102,23 +114,25 @@ function HandleButton() {
                                    height: 8,
                                    transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
                                    "&:before": {
-                                        boxShadow: "0 2px 5px 0 rgba(0,0,0,0.4)"
+                                        boxShadow: "0 2px 5px 0 rgba(0,0,0,0.4)",
                                    },
                                    "&:hover, &.Mui-focusVisible": {
                                         boxShadow: `0px 0px 0px 8px ${theme.palette.mode === "dark" ? "#7743DB" : "#7743DB"
-                                             }`
+                                             }`,
                                    },
                                    "&.Mui-active": {
                                         width: 10,
-                                        height: 10
-                                   }
+                                        height: 10,
+                                   },
                               },
                               "& .MuiSlider-rail": {
-                                   opacity: 0.28
-                              }
+                                   opacity: 0.28,
+                              },
                          }}
                     />
-                    <div className={cx("pl-4 text-[12px] flex items-center text-[#ebeaeac3]")}> - {formatDuration(duration - position)}</div>
+                    <div className={cx('pl-4 text-[12px] flex items-center text-[#ebeaeac3]')}>
+                         - {formatDuration(Math.floor(duration - currentTime))}
+                    </div>
                </div>
           </>
      );
